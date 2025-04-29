e1c3d316293da3850fc9771651e14509f1806478
Mahesh rana <maheshisdelta@gmail.com>
2025-04-30 03:05:12 +0530

    Added Code

---
+++ Portfolio-Website/scripts/script.js
@@ -0,0 +1,25 @@
+let links = document.querySelectorAll("nav a");
+let sections = document.querySelectorAll(".section");
+let home = document.querySelector(".home")
+
+function showSection(el) {
+    console.log(el);
+    sections.forEach((section) => {
+        section.classList.remove("active");
+    });
+    el.classList.add("active"); // Safe addition
+}
+
+
+links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        let id = link.getAttribute("href");
+        let section = document.querySelector(id);
+        if (section) {
+            showSection(section);
+        }
+    });
+});
+
+showSection(home)
